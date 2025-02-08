31f907ab61039fa06b0a5fec8b3127023399581d
Shrisankapali-Cumberlands <skapali32249@ucumberlands.edu>
2025-02-07 23:04:45 -0600

    part 2 basic structures implemented

--- MSCS532_Assignment6.py
+++ MSCS532_Assignment6.py
@@ -90,6 +90,7 @@ sizes = [
     700000,
     800000,
 ]
+
 distributions = ["Sorted", "Reverse_Sorted", "Random"]
 
 # Creating a list to store the execution time for each sort
@@ -168,3 +169,252 @@ plt.title("Deterministic vs Randomized Selection sort to fing 50th element Perfo
 plt.legend()
 plt.grid()
 plt.show()
+
+
+## Part2 - Implementing array, stacks, queues, and linked lists
+## Implementing arrays
+class Array:
+    # Initialize array of fixed size with values as None
+    def __init__(self, size):
+        self.size = size
+        self.array = [None] * size
+
+    # Insert element at array at index
+    def insert(self, index, value):
+        if 0 <= index < self.size:
+            self.array[index] = value
+        else:
+            raise IndexError("Index out of bounds")
+
+    # Delete element from the index, resetting its value to None
+    def delete(self, index):
+        if 0 <= index < self.size:
+            self.array[index] = None
+        else:
+            raise IndexError("Index out of bounds")
+
+    # Get the element at index
+    def access(self, index):
+        if 0 <= index < self.size:
+            return self.array[index]
+        else:
+            raise IndexError("Index out of bounds")
+
+
+## Test cases for array
+print("\n\n************************************")
+print("Test cases for Array")
+print("Initializing an empty array of size 10")
+array = Array(10)
+print("Array initialize of size 10. Current array size = ", array.size)
+print("Current value of index 0 : Expected : None | Actual :", array.access(0))
+
+# Adding elements into array, and accessing their value
+array.insert(0, "First element")
+array.insert(1, "Second element")
+print("Current value of index 0 : Expected : First element | Actual :", array.access(0))
+print(
+    "Current value of index 1 : Expected : Second element | Actual :", array.access(1)
+)
+
+# Removing element 0
+array.delete(0)
+print(
+    "Current value of index 0 after deletion: Expected : None | Actual :",
+    array.access(0),
+)
+
+
+# Implementing stacks from scratch
+class Stack:
+    # Initialize an empty stack
+    def __init__(self):
+        self.stack = []
+
+    # Push an element
+    def push(self, value):
+        self.stack.append(value)
+
+    # Pop the last element
+    def pop(self):
+        # Check if the stack is not empty
+        if not len(self.stack) == 0:
+            return self.stack.pop()
+        else:
+            raise IndexError("Stack is empty")
+
+    # Peek the last element
+    def peek(self):
+        if not len(self.stack) == 0:
+            return self.stack[-1]
+        else:
+            return None
+
+
+## Test cases for stack
+print("\n\n************************************")
+print("Test cases for Stack")
+print("Initializing an empty stack ")
+stack = Stack()
+# Adding elements to stack
+stack.push("First element")
+stack.push("Second element")
+stack.push("Third element")
+# Popping elements from stack
+print("Stack peek", stack.peek())
+print("Stack pop", stack.pop())
+print("Stack pop", stack.pop())
+print("Stack peek", stack.peek())
+
+
+# Implementing Queues
+class Queue:
+    # Initializing an empty queue
+    def __init__(self):
+        self.queue = []
+
+    # Add element to a queue
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    # Remove the first element from queue
+    def dequeue(self):
+        if not len(self.queue) == 0:
+            return self.queue.pop(0)
+        else:
+            return IndexError("Empty queue")
+
+    # Peek the element at index 0
+    def peek(self):
+        if not len(self.queue) == 0:
+            return self.queue[0]
+        else:
+            return None
+
+
+## Test cases for Queue
+print("\n\n************************************")
+print("Test cases for Queue")
+print("Initializing an empty queue")
+queue = Queue()
+# Adding elements to a queue
+queue.enqueue("First element")
+queue.enqueue("Second element")
+queue.enqueue("Third element")
+queue.enqueue("Fourth element")
+# Queue peek
+print("Queue peek", queue.peek())
+# Dequeue
+print("Queue dequeue", queue.dequeue())
+print("Queue dequeue", queue.dequeue())
+print("Queue dequeue", queue.dequeue())
+print("Queue peek", queue.peek())
+
+
+# Implementing Linked List
+# Implementing Node
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def delete(self, value):
+        temp = self.head
+        if temp and temp.value == value:
+            self.head = temp.next
+            temp = None
+            return
+
+        prev = None
+        while temp and temp.value != value:
+            prev = temp
+            temp = temp.next
+
+        if temp is None:
+            return
+
+        prev.next = temp.next
+        temp = None
+
+    def traverse(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+
+## Test cases for Linked List
+print("\n\n************************************")
+print("Test cases for Linked List")
+print("Initializing an empty Linked List")
+linkedList = LinkedList()
+print("Empty linked list. Expected: None | Actual: ", linkedList.traverse())
+# Adding value to linked list
+linkedList.insert(1)
+linkedList.insert(2)
+linkedList.insert(3)
+linkedList.insert(4)
+print("Adding 4 entries. Calling the traverse function")
+print(linkedList.traverse())
+
+print("Removing 2. The new list is")
+linkedList.delete(2)
+print(linkedList.traverse())
+
+
+# Rooted Trees
+class TreeNode:
+    # Initialize a tree with value and childrens
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+    # Add new child
+    def add_child(self, child):
+        self.children.append(child)
+
+    # Traverse
+    def traverse(self, level=0):
+        print(" " * level + str(self.value))
+        for child in self.children:
+            child.traverse(level + 1)
+
+
+## Test cases for Tree
+print("\n\n************************************")
+print("Test cases for Tree")
+print("Initializing an empty tree")
+# Initializing root tree
+root = TreeNode("Root")
+# Create child roots
+child1 = TreeNode("Child1")
+child2 = TreeNode("Child2")
+child3 = TreeNode("Child3")
+# Add children to root tree
+root.add_child(child1)
+root.add_child(child2)
+root.add_child(child3)
+# Add sub child to child trees
+child1.add_child(TreeNode("Child1.1"))
+child1.add_child(TreeNode("Child1.2"))
+child2.add_child(TreeNode("Child2.1"))
+child2.add_child(TreeNode("Child2.2"))
+child3.add_child(TreeNode("Child3.1"))
+child3.add_child(TreeNode("Child3.2"))
+
+print("Tree Structure:")
+root.traverse()
